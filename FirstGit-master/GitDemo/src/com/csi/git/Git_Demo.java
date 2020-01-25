5b0429ebfbde6500f13d333ceb7b18e45814d889
venkatesh <venkatesh@LAPTOP-F8R1JJKI>
2020-01-25 13:44:37 +0530

    changed to lambda

--- FirstGit-master/GitDemo/src/com/csi/git/Git_Demo.java
+++ FirstGit-master/GitDemo/src/com/csi/git/Git_Demo.java
@@ -5,6 +5,8 @@ public class Git_Demo {
 	public static void main(String[] args) {
 	System.out.println("Hello World");
 
+	System.out.println("Changes made by me");
+	System.out.println("changes second");
 	}
 
 }
---
+++ githubdemo-master/Demo_Git_HUb/src/com/csi/git/Hello_World_Basic.java
@@ -0,0 +1,13 @@
+package com.csi.git;
+interface A111
+{
+	public void view(int a);}
+public class Hello_World_Basic {
+
+	public static void main(String[] args) {
+		System.out.println("CSI, PUNE");
+		A111 a=(int u)->{System.out.println(u);};
+		a.view(12);
+	}
+
+}
